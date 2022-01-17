4351d921cd0c7e53057a2648c96403591177bb98
Yoshinori Oomuro <39080437+yomuro9@users.noreply.github.com>
2022-01-17 16:34:13 +0900

    Update application.py

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ def hello():
 
 
 @app.route("/info")
-def profile():
+def info():
     print("[pool-3-thread-1] INFO  [SimpleJobLauncher.java:133 run] - [Job: [SimpleJob: [name=jobXXXThread]] launched with the following parameters: [{time=1583247660000}]]")
     return "infoのログ
 
