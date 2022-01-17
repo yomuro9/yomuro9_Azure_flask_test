f0109b4a1d09a019fe8343b5bb1935bd0c9eb738
Yoshinori Oomuro <39080437+yomuro9@users.noreply.github.com>
2022-01-17 13:56:21 +0900

    Create application.py

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/profilr/<string:user_name>")
+def profile(user_name):
+    return "{name}さんのプロフィール画面です".format(name=user_name)
+
+
+if __name__ == "__main__":
+    app.run()
